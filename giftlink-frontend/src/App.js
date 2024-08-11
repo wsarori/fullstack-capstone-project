9ea6d5bbd0266a2745ea7d2c61d14948fc94964e
wsarori <wsarori@gmail.com>
2024-08-11 10:10:16 -0400

    Your message here

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -6,6 +6,7 @@ import RegisterPage from './components/RegisterPage/RegisterPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import SearchPage from './components/SearchPage/SearchPage';
 function App() {
   const navigate = useNavigate();
   return (
